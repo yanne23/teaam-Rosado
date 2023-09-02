1490813a81ff5d938159aae99f3e262731e7a8b9
yanne23 <yannella_lopez@usmp.pe>
2023-09-02 03:31:19 -1200

    ...

--- juego2D-Grupo Rosado/Assets/Content/Scrpit/CharacterControll.cs
+++ juego2D-Grupo Rosado/Assets/Content/Scrpit/CharacterControll.cs
@@ -6,11 +6,21 @@ public class CharacterControll : MonoBehaviour
 {
     public float velocidad;
     public float fuerzaSalto;
+    public float saltosMaximos;
+    public LayerMask capaSuelo;
+
+
     private Rigidbody2D rigiBody;
+    private BoxCollider2D boxCollider;
     private bool mirandoDerecha = true;
+    private float saltosRestantes;
+    private Animator animator;
 
     public void Start(){
         rigiBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        saltosRestantes = saltosMaximos;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,17 +29,38 @@ public class CharacterControll : MonoBehaviour
         ProcesarSalto();
     }
 
+    bool EstaEnSuelo(){
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.1f, capaSuelo);
+        return raycastHit.collider != null;  //OverlapCircle(transform.position, 0.5f, LayerMask.GetMask("Ground"));
+    }
+
     void ProcesarSalto(){
-        if(Input.GetKeyDown(KeyCode.Space)){
+
+        if(EstaEnSuelo()){
+            saltosRestantes = saltosMaximos;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0){
+            saltosRestantes--;
+            rigiBody.velocity = new Vector2(rigiBody.velocity.x, 0f);
             rigiBody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
         }
     
     }
 
+
     void ProcesandoMovimiento()
     {
        //logica del movimiento
        float inputMovimiento = Input.GetAxis("Horizontal");
+
+        if(inputMovimiento != 0f){
+            animator.SetBool("isRunning", true);
+        }else{
+            animator.SetBool("isRunning", false);
+        }
+
        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
        rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y);
