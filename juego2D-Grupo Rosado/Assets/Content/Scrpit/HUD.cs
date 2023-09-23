1d6f5a3a62d4e076a564536325eb3da4a05a7d92
yanne23 <yannella_lopez@usmp.pe>
2023-09-22 21:21:12 -1200

    nivel 1
    nivel 1 - casi terminado
    falta agregar enemigos
    falta nivel 2
    creditos y un mini menu

--- juego2D-Grupo Rosado/Assets/Content/Scrpit/HUD.cs
+++ juego2D-Grupo Rosado/Assets/Content/Scrpit/HUD.cs
@@ -7,8 +7,38 @@ public class HUD : MonoBehaviour
     public GameManagerScript gameManager;
     public TextMeshProUGUI puntos;
 
+     public int counterTime= 300;
+    public TextMeshProUGUI counterTimeText;
+
+    public int enemy = 0;
+    public TextMeshProUGUI enemyText;
+
+
     void Update()
     {
         puntos.text = gameManager.PuntosTotales.ToString();
     }
+
+     void Start()
+    {
+        counterTimeText.text = string.Format("{0}", counterTime);
+        InvokeRepeating("TimeCounter", 1f, 1f);
+    }
+
+    public void EnemyCounter(){
+        enemy++;
+        enemyText.text = string.Format("{0}", enemy);
+    }
+
+    public void TimeCounter(){
+        counterTime--;
+        if (counterTime<0){
+            //desabilitar al player
+            //enviar el mensaje de game over (habilitarlo)
+            Debug.Log("Game Over");
+        }
+        counterTimeText.text = string.Format("{0}", counterTime);
+    }
+
+
 }
